825acfb7e2daa0a55df4afe8cf915420ca0a97e9
KARUNAKARAN V VIJAYARAGHAVAN <66834895+karunakaran186@users.noreply.github.com>
2022-03-05 00:32:20 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,9 +3,11 @@ import pyttsx3
 import pywhatkit
 import pyjokes
 import wikipedia
-from geopy.geocoders import Nominatim
-from bs4 import BeautifulSoup
+import webbrowser
 import requests
+from email.mime import audio
+from numpy import place
+from setuptools import Command
 
 
 listener = sr.Recognizer()
@@ -14,11 +16,9 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
 
-
 def talk(text):
     engine.say(text)
     engine.runAndWait()
-
 
 
 hi = 0
@@ -39,117 +39,134 @@ else:
 
 
 def take_command():
-    try:
-        with sr.Microphone() as source:
-            talk('listening.....')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'kavi' in command:
-                command = command.replace('kavi', '')
-                print(command)
-    except:
-        pass
-    return command
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        talk("listening.....")
+        audio = r.record(source, duration=3)
 
-def run_kavi():
-    command = take_command()
-    print(command)
+        try:
+            command = r.recognize_google(audio, language='en-in')
+            print(f"user said:{command}\n")
 
-    if 'play' in command:
-        talk('playing')
-        print('playing')
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'whatsapp' in command:
-        pywhatkit.sendwhatmsg("+91 93611 40968", "hello iam kavi,my boss has told me to text any important info",
-                              19, )
-        print("Successfully Sent!")
-    elif 'who is' in command:
-        person = command.replace('who is', '')
-        source = wikipedia.summary(person, 20)
-        print(source)
-        talk(source)
-    elif 'search' in command:
-        info = command.replace('search', '')
-        general = wikipedia.search(info, 20)
-        print(general)
-        talk(general)
-    elif 'history' in command:
-        gen = command.replace('history, battle, movie review', '')
-        small = wikipedia.summary(gen, 20)
-        print(small)
-        talk(small)
-    elif 'location' in command:
-        loc = Nominatim(user_agent="GetLoc")
-        getloc = loc.geocode("Coimbatore")
-        print(getloc.address)
-        talk(getloc)
-    elif 'weather' in command:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        except Exception as e:
+            talk("Pardon me,Please say that again")
+            print("Pardon me,Please say that again")
+            return "Not Found"
+            print("command")
+        return command
 
-        def weather(city):
-            city = city.replace(" ", "+")
-            res = requests.get(
-                f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',
-                headers=headers)
-            print("Searching...\n")
-            soup = BeautifulSoup(res.text, 'html.parser')
-            location = soup.select('#wob_loc')[0].getText().strip()
-            time = soup.select('#wob_dts')[0].getText().strip()
-            info = soup.select('#wob_dc')[0].getText().strip()
-            weather = soup.select('#wob_tm')[0].getText().strip()
-            print(location)
+
+print("Loading your AI personal Assistant kavi")
+talk("Loading your AI personal Assistant kavi")
+
+if __name__ == '__main__':
+
+    while True:
+        talk("Tell me Sir! How can I help you?")
+        print("Tell me Sir! How can I help you?")
+        command = take_command().lower()
+
+        if "exit" in command or "stop" in command or "shutdown" in command:
+            talk("Your AI assistant kavi is shutting down,Good bye Sir and have a good day (:")
+            print("Your AI assistant kavi is shutting down,Good bye Sir and have a good day (:")
+            break
+
+        elif 'play' in command:
+            talk('playing')
+            print('playing')
+            song = command.replace('play', '')
+            talk('playing ' + song)
+            pywhatkit.playonyt(song)
+
+        elif 'whatsapp' in command:
+            pywhatkit.sendwhatmsg("+91 93611 40968", "hello iam kavi,my boss has told me to text any important info",
+                                  13, 58)
+            print("Successfully Sent!")
+            continue
+
+        elif 'who is' in command:
+            person = command.replace('who is', '')
+            source = wikipedia.summary(person, 100)
+            print(source)
+            talk(source)
+
+
+        elif 'search' in command:
+            info = command.replace('search', '')
+            general = wikipedia.search(info, 100)
+            print(general)
+            talk(general)
+
+        elif 'history' in command:
+            gen = command.replace('history, battle, movie review', '')
+            small = wikipedia.summary(gen, 100)
+            print(small)
+            talk(small)
+
+        elif 'movie review' in command:
+            movie = command.replace('movie review', '')
+            small = wikipedia.summary(movie, 10)
+            print(small)
+            talk(small)
+
+        elif 'are you single' in command:
+            talk('no......um.i am in relationship with wireless devices')
+        elif 'do you like me' in command:
+            talk('yes boss definitely')
+        elif 'what is your name' in command:
+            talk('My devloper karunakran has named me kkavi')
+        elif 'cringe' in command:
+            talk('alright........he/she was funniest perosn')
+        elif 'joke' in command:
+            joke = pyjokes.get_joke()
+            print(joke)
+            talk(joke)
+        elif 'i am tired' in command:
+            talk('you should take a break')
+        elif 'favorite game' in command:
+            talk('my favorite game is chess')
+        elif 'can you dance' in command:
+            talk('I cant dance as of now, but I can play some dance music')
+        elif 'how do i look' in command:
+            talk('juding from your voice, amazing')
+        elif 'can you cook' in command:
+            talk('i can cook you up amazing bedtime stories if you want')
+        elif 'who is your friend' in command:
+            talk('her name is nilla voice assistant, she was in another repository')
+
+        elif "where is" in command:
+            command = command.replace("where is", "")
+            location = command
+            talk("User asked to Locate")
             talk(location)
-            print(info)
-            talk(info)
-            print(time)
-            talk(time)
-            ab = (weather + "°C")
-            talk(ab)
-            print(ab)
+            webbrowser.open("https://www.google.nl/maps/place/" + location + "")
 
-        city1 = "coimbatore"
-        talk(city1)
-        city = city1 + " weather"
-        weather(city)
-        abc = ("Have a Nice Day Buddy")
-        talk(abc)
-
-    elif 'movie review' in command:
-        movie = command.replace('movie review', '')
-        small = wikipedia.summary(movie, 10)
-        print(small)
-        talk(small)
-    elif 'are you single' in command:
-        talk('no......um.i am in relationship with wireless devices')
-    elif 'do you like me' in command:
-        talk('yes boss definitely')
-    elif 'what is your name' in command:
-        talk('My devloper karunakran has named me kkavi')
-    elif 'cringe' in command:
-        talk('alright........he/she was funniest personn')
-    elif 'joke' in command:
-        joke = pyjokes.get_joke()
-        print(joke)
-        talk(joke)
-    elif 'i am tired' in command:
-        talk('you should take a break')
-    elif 'favorite game' in command:
-        talk('my favorite game is chess')
-    elif 'can you dance' in command:
-        talk('I cant dance as of now, but I can play some dance music')
-    elif 'how do i look' in command:
-        talk('juding from your voice, amazing')
-    elif 'can you cook' in command:
-        talk('i can cook you up amazing bedtime stories if you want')
-    elif 'who is your friend' in command:
-        talk('her name is nilla voice assistant, she was in another repository')
-    else:
-        talk('cant get it....please say it again')
+        elif "weather" in command:
+            api_key = "51d5d78391e312e72cde67174f38e770"
+            base_url = "https://api.openweathermap.org/data/2.5/weather?"
+            talk("which city are you looking to know")
+            place = take_command()
+            complete_url = base_url + "appid=" + api_key + "&q=" + place
+            result = requests.get(complete_url)
+            x = result.json()
+            if x["cod"] != "404":
+                y = x["main"]
+                city_temperature = y["temp"]
+                city_humidiy = y["humidity"]
+                ans = x["weather"]
+                weather_description = ans[0]["description"]
+                talk("Temperature" + str(place) + "in kelvin unit is " +
+                     str(city_temperature) +
+                     "\n humidity in percentage is " +
+                     str(city_humidiy) +
+                     "\n description  " +
+                     str(weather_description))
+                print(str(place) + " Temperature in kelvin unit = " +
+                      str(city_temperature) +
+                      "\n humidity (in percentage) = " +
+                      str(city_humidiy) +
+                      "\n description = " +
+                      str(weather_description))
 
 
-while True:
-    run_kavi()
